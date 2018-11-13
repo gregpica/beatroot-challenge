31b145e47f60c40747a93f0654a738267f49a2ce
gregpica <gpica@bu.edu>
2018-11-13 12:37:57 -0800

    Add get_tracks method to BeatRootService class

--- app/services/beat_root_service.rb
+++ app/services/beat_root_service.rb
@@ -6,4 +6,8 @@ class BeatRootService
     @token = params[:token]
   end
 
+  def get_tracks
+    api_path = "#{BASE_URL}/accounts/#{@account}/tracks"
+    RestClient.get(api_path, :Authorization => "Token token=#{@token}")
+  end 
 end
