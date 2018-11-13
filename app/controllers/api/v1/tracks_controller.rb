3c9f949993ee84237f463fe6396f1aa1c46b333e
gregpica <gpica@bu.edu>
2018-11-13 15:25:22 -0800

    Create show action method for tracks API controller

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -4,4 +4,11 @@ class Api::V1::TracksController < ApiController
     response = JSON.parse(client.get_all_tracks)
     render json: response
   end
+
+  def show
+    client = BeatRootService.new({account: ENV['BEATROOT_ACCOUNT'], token: ENV['BEATROOT_TOKEN']})
+    track_id = params[:id]
+    response = JSON.parse(client.get_track(track_id))
+    render xml: response["track"].to_xml(root: "Track")
+  end
 end
