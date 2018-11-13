843c38a8e561cc9528bf84a581be1da5b49649ea
gregpica <gpica@bu.edu>
2018-11-13 14:46:24 -0800

    Create rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Rails.application.routes.draw do
+  root 'tracks#index'
+
+  resources :tracks, only: [:index]
+
+  namespace :api do
+    namespace :v1 do
+      resources :tracks, only: [:index, :show]
+    end
+  end
 end
