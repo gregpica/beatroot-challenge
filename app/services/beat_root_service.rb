3eefeaeba042c4f6df55b1d0988a9e6e65049e95
gregpica <gpica@bu.edu>
2018-11-13 12:50:59 -0800

    Rename get_tracks for clarity and DRY up BeatRootService class

--- app/services/beat_root_service.rb
+++ app/services/beat_root_service.rb
@@ -6,13 +6,19 @@ class BeatRootService
     @token = params[:token]
   end
 
-  def get_tracks
+  def get_all_tracks
     api_path = "#{BASE_URL}/accounts/#{@account}/tracks"
-    RestClient.get(api_path, :Authorization => "Token token=#{@token}")
+    rest_client_get(api_path)
   end
 
   def get_track(id)
     api_path = "#{BASE_URL}/accounts/#{@account}/tracks/#{id}"
+    rest_client_get(api_path)
+  end
+
+  private
+
+  def rest_client_get(api_path)
     RestClient.get(api_path, :Authorization => "Token token=#{@token}")
-  end 
+  end
 end
