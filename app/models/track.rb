6eb57d43b9f1d41dcbe819d05fb254b2b7a6b906
gregpica <gpica@bu.edu>
2018-11-14 19:13:15 -0800

    Create Track model

---
+++ app/models/track.rb
@@ -0,0 +1,15 @@
+class Track
+  attr_accessor(
+     :isrc,
+     :title,
+     :subtitle,
+     :duration,
+     :artist_name,
+     :direct_contributors,
+     :indirect_contributors,
+     :record_label_names,
+     :p_line,
+     :genres,
+     :parental_warning_type
+  )
+end
